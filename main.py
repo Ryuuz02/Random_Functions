a021dcf6ea0ac7ce11d5893644bd90692dbf0b52
Ryuuz02 <ryuuz02@outlook.com>
2023-03-24 01:34:32 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+def yes_or_no(question):
+    reply = str(input(question+' (y/n): ')).lower().strip()
+    if reply[0] == 'y':
+        return True
+    if reply[0] == 'n':
+        return False
+# print(yes_or_no('Is it a good day?'))
+
